109fcccfce72bd76a942734a5661eac4bb9aa919
Galeev LXIX <gtimur477@gmail.com>
2025-02-13 18:25:09 +0300

    Добавьте файлы проекта.

---
+++ DX12GE/DX12GE.cpp
@@ -0,0 +1,42 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <Shlwapi.h>
+
+#include "Engine/Application.h"
+#include "BianGame.h"
+
+#include <dxgidebug.h>
+
+void ReportLiveObjects()
+{
+    IDXGIDebug1* dxgiDebug;
+    DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug));
+
+    dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_IGNORE_INTERNAL);
+    dxgiDebug->Release();
+}
+
+int CALLBACK main(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
+{
+    int retCode = 0;
+
+    // Set the working directory to the path of the executable.
+    WCHAR path[MAX_PATH];
+    HMODULE hModule = GetModuleHandleW(NULL);
+    if (GetModuleFileNameW(hModule, path, MAX_PATH) > 0)
+    {
+        PathRemoveFileSpecW(path);
+        SetCurrentDirectoryW(path);
+    }
+
+    Application::Create(hInstance);
+    {
+        std::shared_ptr<BianGame> demo = std::make_shared<BianGame>(L"Bian Game", 1280, 720);
+        retCode = Application::Get().Run(demo);
+    }
+    Application::Destroy();
+
+    atexit(&ReportLiveObjects);
+
+    return retCode;
+}
---
+++ DX12GE/Engine/Helpers.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h> // For HRESULT
+
+  // From DXSampleHelper.h 
+  // Source: https://github.com/Microsoft/DirectX-Graphics-Samples
+inline void ThrowIfFailed(HRESULT hr)
+{
+    if (FAILED(hr))
+    {
+        throw std::exception();
+    }
+}
+
+#define STR1(x) #x
+#define STR(x) STR1(x)
+#define WSTR1(x) L##x
+#define WSTR(x) WSTR1(x)
+#define NAME_D3D12_OBJECT(x) x->SetName( WSTR(__FILE__ "(" STR(__LINE__) "): " L#x) )
