ae0168a8a70f9e1c55dbc451e47146599eef03e5
Zakgard <mr.zakhar21@gmail.com>
2026-01-21 01:01:07 +0300

    scripts update

--- DX12GE/Engine/Lua/LuaManager.cpp
+++ DX12GE/Engine/Lua/LuaManager.cpp
@@ -24,6 +24,7 @@ static NodeGraphSystem* p_grapsh_system;
 static std::vector<std::string> lua_classes_vector;
 static std::vector<std::string> lua_file_classes;
 static std::map<std::string, std::vector<std::string>> lua_classes_map;
+static std::map<std::string, std::vector<std::string>> node_to_classes;
 static sol::state lua;
 
 //////////////////////////////////////////////////////////////////////
@@ -120,6 +121,12 @@ Node3D* lua_get_child(Node3D* object, std::string childName)
 	assert(object != nullptr, "Attempt to call get child to on null object!");
 
 	return object->GetChild(childName);
+}
+
+int lua_change_color(Object3DNode* object, float r, float g, float b)
+{
+	//object->MaterialsOverride
+	return 1;
 }
 
 Node3D* lua_get_parent(Node3D* object)
@@ -296,9 +303,8 @@ std::vector<std::string> FindAllLuaFiles(const std::string& rootPath,
 	return result;
 }
 
-LuaManager::LuaManager()
+void LuaManager::LoadScrtipts()
 {
-	//sol::state lua;
 	if (EngineConfig::IsReleaseMode)
 	{
 
@@ -306,10 +312,8 @@ LuaManager::LuaManager()
 	else
 	{
 		lua.open_libraries(sol::lib::base, sol::lib::coroutine, sol::lib::string, sol::lib::io);
-
 		lua.set_function("Register", &lua_register_class);
 		lua.set_function("LoadObjectWithModel", &lua_load_object_with_model);
-		//	lua.set_function("GetCamera", &lua_get_camera);
 		lua.set_function("RotateBy", &lua_rotate_object_by_rotator);
 		lua.set_function("TranslateTo", &lua_transform_move_to);
 		lua.set_function("GetObjectOnScene", &lua_get_object_on_scene);
@@ -318,7 +322,7 @@ LuaManager::LuaManager()
 		lua.set_function("TranslateBy", &lua_transform_move_by);
 		lua.set_function("CallError", &lua_call_assert);
 		lua.set_function("GetVelocity", &lua_get_velocity);
-		lua.set_function("AddImpule", &lua_add_impulse);
+		lua.set_function("AddImpulse", &lua_add_impulse);
 		lua.set_function("GetNodeType", &lua_get_node_type);
 		lua.set_function("GetWorldDirection", &lua_get_object_world_direction);
 		lua.set_function("GetChild", &lua_get_child);
@@ -334,7 +338,7 @@ LuaManager::LuaManager()
 		for (size_t i = 0; i < luaFiles.size(); ++i) {
 			std::cout << "  [" << (i + 1) << "/" << count << "] " << luaFiles[i] << std::endl;
 			size_t lastSlash = luaFiles[i].find_last_of("/\\");
-			if (lastSlash != std::string::npos) 
+			if (lastSlash != std::string::npos)
 			{
 				lua_file_classes.push_back(luaFiles[i].substr(lastSlash + 1));
 			}
@@ -345,6 +349,23 @@ LuaManager::LuaManager()
 			lua.safe_script_file(luaFiles[i]);
 		}
 	}
+}
+
+/*void LuaManager::CallCollision(int32_t ObjectID1, uint32_t ObjectID2)
+{
+	Object3DNode* node_one = p_grapsh_system->GetObjectByID(ObjectID1);
+	std::vector<std::string> scripts = node_one->GetNodeScripts();
+
+	//sol::table temp_class = lua[lua_class];
+	//temp_class["OnMouseMovementInputReceived"](temp_class, e.X, e.Y);
+
+	Object3DNode* node_two = p_grapsh_system->GetObjectByID(ObjectID2);
+}*/
+
+LuaManager::LuaManager()
+{
+	//sol::state lua;
+	LoadScrtipts();
 }
 
 LuaManager::~LuaManager()
@@ -450,7 +471,11 @@ std::string LuaManager::CreateValidClass(std::string className, std::string objI
 
 void LuaManager::ReloadScripts()
 {
-
+	lua_classes_vector.clear();
+	lua_file_classes.clear();
+	lua_classes_map.clear();
+	lua.~state();
+	lua = sol::state();
 }
 
 void LuaManager::StartScript(std::string className)
